6faae07318a5ad2dcf69fc3440004ac681bdeb99
parasshah10 <116607050+parasshah10@users.noreply.github.com>
2023-07-15 04:44:57 +0530

    Update Footer.js

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -43,13 +43,13 @@ const Footer = () => {
             >
               {systemName} {process.env.REACT_APP_VERSION}{' '}
             </a>
-            由{' '}
+            Depend on{' '}
             <a href='https://github.com/songquanpeng' target='_blank'>
               JustSong
             </a>{' '}
-            构建，源代码遵循{' '}
+            To build, the source code follows{' '}
             <a href='https://opensource.org/licenses/mit-license.php'>
-              MIT 协议
+              MIT protocol
             </a>
           </div>
         )}
