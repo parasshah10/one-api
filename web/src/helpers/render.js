2613141b3d49b876691ed69abf3f9582a7d3a812
parasshah10 <116607050+parasshah10@users.noreply.github.com>
2023-07-15 04:27:57 +0530

    Update render.js

--- web/src/helpers/render.js
+++ web/src/helpers/render.js
@@ -52,7 +52,7 @@ export function renderQuotaWithPrompt(quota, digits) {
   let displayInCurrency = localStorage.getItem('display_in_currency');
   displayInCurrency = displayInCurrency === 'true';
   if (displayInCurrency) {
-    return `（等价金额：${renderQuota(quota, digits)}）`;
+    return `（Equivalent amount：${renderQuota(quota, digits)}）`;
   }
   return '';
 }
