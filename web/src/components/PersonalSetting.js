4216bd1ff7c5426388cd4c9a12f59a3cb1e11603
parasshah10 <116607050+parasshah10@users.noreply.github.com>
2023-07-15 06:17:38 +0530

    Update PersonalSetting.js

--- web/src/components/PersonalSetting.js
+++ web/src/components/PersonalSetting.js
@@ -39,7 +39,7 @@ const PersonalSetting = () => {
     const { success, message, data } = res.data;
     if (success) {
       await copy(data);
-      showSuccess(`令牌已重置并已复制到剪贴板：${data}`);
+      showSuccess(`Token was reset and copied to clipboard：${data}`);
     } else {
       showError(message);
     }
@@ -51,7 +51,7 @@ const PersonalSetting = () => {
     if (success) {
       let link = `${window.location.origin}/register?aff=${data}`;
       await copy(link);
-      showNotice(`邀请链接已复制到剪切板：${link}`);
+      showNotice(`Invite link copied to clipboard：${link}`);
     } else {
       showError(message);
     }
@@ -64,7 +64,7 @@ const PersonalSetting = () => {
     );
     const { success, message } = res.data;
     if (success) {
-      showSuccess('微信账户绑定成功！');
+      showSuccess('The WeChat account is bound successfully!');
       setShowWeChatBindModal(false);
     } else {
       showError(message);
@@ -80,7 +80,7 @@ const PersonalSetting = () => {
   const sendVerificationCode = async () => {
     if (inputs.email === '') return;
     if (turnstileEnabled && turnstileToken === '') {
-      showInfo('请稍后几秒重试，Turnstile 正在检查用户环境！');
+      showInfo('Please try again in a few seconds, Turnstile is checking the user environment!');
       return;
     }
     setLoading(true);
@@ -89,7 +89,7 @@ const PersonalSetting = () => {
     );
     const { success, message } = res.data;
     if (success) {
-      showSuccess('验证码发送成功，请检查邮箱！');
+      showSuccess('The verification code has been sent successfully, please check your email!');
     } else {
       showError(message);
     }
@@ -104,7 +104,7 @@ const PersonalSetting = () => {
     );
     const { success, message } = res.data;
     if (success) {
-      showSuccess('邮箱账户绑定成功！');
+      showSuccess('The email account is bound successfully!');
       setShowEmailBindModal(false);
     } else {
       showError(message);
@@ -114,17 +114,17 @@ const PersonalSetting = () => {
 
   return (
     <div style={{ lineHeight: '40px' }}>
-      <Header as='h3'>通用设置</Header>
+      <Header as='h3'>General Settings</Header>
       <Message>
-        注意，此处生成的令牌用于系统管理，而非用于请求 OpenAI 相关的服务，请知悉。
+        Note that the token generated here is used For system management, not For requesting OpenAI-related services, please be aware.
       </Message>
       <Button as={Link} to={`/user/edit/`}>
-        更新个人信息
+        Update Personal Information
       </Button>
-      <Button onClick={generateAccessToken}>生成系统访问令牌</Button>
-      <Button onClick={getAffLink}>复制邀请链接</Button>
+      <Button onClick={generateAccessToken}>Generate System Access Token</Button>
+      <Button onClick={getAffLink}>Copy Invite Link</Button>
       <Divider />
-      <Header as='h3'>账号绑定</Header>
+      <Header as='h3'>Account Binding</Header>
       {
         status.wechat_login && (
           <Button
@@ -132,7 +132,7 @@ const PersonalSetting = () => {
               setShowWeChatBindModal(true);
             }}
           >
-            绑定微信账号
+            Bind WeChat account
           </Button>
         )
       }
@@ -147,19 +147,19 @@ const PersonalSetting = () => {
             <Image src={status.wechat_qrcode} fluid />
             <div style={{ textAlign: 'center' }}>
               <p>
-                微信扫码关注公众号，输入「验证码」获取验证码（三分钟内有效）
+                Scan the WeChat code to follow the official account, enter the "verification code" (valid within three minutes)
               </p>
             </div>
             <Form size='large'>
               <Form.Input
                 fluid
-                placeholder='验证码'
+                placeholder='Verification Code'
                 name='wechat_verification_code'
                 value={inputs.wechat_verification_code}
                 onChange={handleInputChange}
               />
               <Button color='' fluid size='large' onClick={bindWeChat}>
-                绑定
+                Bind
               </Button>
             </Form>
           </Modal.Description>
@@ -167,7 +167,7 @@ const PersonalSetting = () => {
       </Modal>
       {
         status.github_oauth && (
-          <Button onClick={openGitHubOAuth}>绑定 GitHub 账号</Button>
+          <Button onClick={openGitHubOAuth}>Bind GitHub account</Button>
         )
       }
       <Button
@@ -175,7 +175,7 @@ const PersonalSetting = () => {
           setShowEmailBindModal(true);
         }}
       >
-        绑定邮箱地址
+        Bind Email Address
       </Button>
       <Modal
         onClose={() => setShowEmailBindModal(false)}
@@ -184,25 +184,25 @@ const PersonalSetting = () => {
         size={'tiny'}
         style={{ maxWidth: '450px' }}
       >
-        <Modal.Header>绑定邮箱地址</Modal.Header>
+        <Modal.Header>Bind Email Address</Modal.Header>
         <Modal.Content>
           <Modal.Description>
             <Form size='large'>
               <Form.Input
                 fluid
-                placeholder='输入邮箱地址'
+                placeholder='Enter Email Address'
                 onChange={handleInputChange}
                 name='email'
                 type='email'
                 action={
                   <Button onClick={sendVerificationCode} disabled={loading}>
-                    获取验证码
+                    Get Verification Code
                   </Button>
                 }
               />
               <Form.Input
                 fluid
-                placeholder='验证码'
+                placeholder='Verification Code'
                 name='email_verification_code'
                 value={inputs.email_verification_code}
                 onChange={handleInputChange}
@@ -224,7 +224,7 @@ const PersonalSetting = () => {
                 onClick={bindEmail}
                 loading={loading}
               >
-                绑定
+                Bind
               </Button>
             </Form>
           </Modal.Description>
